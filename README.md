67bd1dee18617ab36d16149e9b2a300575ca35f0
abhinav gaurav <abhinavgaurav001@gmail.com>
2021-05-20 01:36:10 +0530

    enrich assignment

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # enrich
+
+<!-- 1st run  adapter.js which consist of server socket which will send response to client socket -->
+
+<!-- 2nd run node simulator.js which consist of client socket -->
+
+<!-- 3rd packets.js consist of every packet value  -->
+
+<!-- 4th packetTypes.js consist of tyoes of packets used -->
+
+<!-- Running procedure: -->
+node adapter.js 
+<!-- will run on 7070 -->
+node simulator.js
+<!-- both server will run on different terminal at same time -->
---
+++ simulator.js
@@ -0,0 +1,57 @@
+const net = require("net");
+const client = new net.Socket();
+const port = 7070;
+const host = "127.0.0.1";
+const { loginPacket,heartbeatPacket, gpsPacket } = require('./packets.js');
+const { PacketType } = require('./packetTypes')
+
+let loginInterval;
+let heartbeatInterval;
+//client connection
+client.connect(port, host, function() {
+  console.log("Connected",loginPacket);
+  // client.write("Hello From Client " + client.address().address);
+
+  loginInterval = setInterval(() => {
+    client.write(loginPacket);
+  }, 1000);
+
+});
+//client on getting data
+client.on("data", function(data) {
+  let protocolNumber = data.slice(3,4);
+    console.log(protocolNumber.toString('hex'));
+  switch (protocolNumber.toString('hex')) {
+    case PacketType.LoginInformation:
+      console.log("Server Says Login Success : ");
+
+      //stop sending login packets
+      clearInterval(loginInterval);
+    
+      //send heartbeat packets
+      console.log('send heartbeat packets');
+      heartbeatInterval = setInterval(()=>{
+        client.write(heartbeatPacket)
+        client.write(gpsPacket)
+      },1000);
+        break;
+    case PacketType.HeartbeatPacket:
+        console.log('heartbeat packet response received');
+        break;
+    default:
+        console.log('default');
+        
+        break;
+}
+  
+});
+//client error
+client.on('error', function(err){
+  console.log(err);
+  
+})
+//close client
+client.on("close", function() {
+  clearInterval(heartbeatInterval);
+  console.log("Connection closed");
+});
